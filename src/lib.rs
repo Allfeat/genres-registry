505c6586af971a199d458355139021f23ede2b27
SailorSnoW <sailorsnow@pm.me>
2023-10-03 02:51:47 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,149 @@
+// This file is part of Allfeat.
+
+// Copyright (C) Allfeat (FR) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pub mod macros;
+
+use codec::{Decode, Encode, MaxEncodedLen};
+use scale_info::TypeInfo;
+use sp_runtime::RuntimeDebug;
+
+declare_music_genre! {
+    Classical(ClassicalSubtype {
+        Baroque,
+        Romantic,
+        Modern,
+        Contemporary,
+        ChamberMusic,
+        Symphony,
+    }),
+    Jazz(JazzSubtype {
+        Bebop,
+        Fusion,
+        Smooth,
+        Swing,
+        Cool,
+        FreeJazz,
+    }),
+    Rock(RockSubtype {
+        ClassicRock,
+        HardRock,
+        AlternativeRock,
+        ProgressiveRock,
+        PunkRock,
+        Psychedelic,
+    }),
+    Electronic(ElectronicSubtype {
+        Techno,
+        House,
+        Trance,
+        Ambient,
+        DrumNBass,
+        IDM,
+    }),
+    Rap(RapSubtype {
+        EastCoast,
+        WestCoast,
+        Trap,
+        Gangsta,
+        Conscious,
+        AlternativeRap,
+    }),
+    Pop(PopSubtype {
+        SynthPop,
+        DancePop,
+        TeenPop,
+        BritPop,
+        ArtPop,
+    }),
+    Country(CountrySubtype {
+        Bluegrass,
+        HonkyTonk,
+        OutlawCountry,
+        CountryRock,
+        Contemporary,
+    }),
+    Blues(BluesSubtype {
+        DeltaBlues,
+        ChicagoBlues,
+        ElectricBlues,
+        RhythmAndBlues,
+    }),
+    Soul(SoulSubtype {
+        NeoSoul,
+        BlueEyedSoul,
+        NorthernSoul,
+        Motown,
+    }),
+    Reggae(ReggaeSubtype {
+        Dancehall,
+        Dub,
+        RootsReggae,
+    }),
+    Folk(FolkSubtype {
+        TraditionalFolk,
+        ContemporaryFolk,
+        FolkRock,
+    }),
+    RnB(RnBSubtype {
+        ContemporaryRnB,
+        NewJackSwing,
+        Funk,
+    }),
+    Metal(MetalSubtype {
+        HeavyMetal,
+        DeathMetal,
+        BlackMetal,
+        PowerMetal,
+        ThrashMetal,
+        NuMetal,
+        DoomMetal,
+        GothicMetal,
+    }),
+    Punk(PunkSubtype {
+        HardcorePunk,
+        PostPunk,
+        AnarchoPunk,
+        PopPunk,
+    }),
+    Indie(IndieSubtype {
+        IndieRock,
+        IndiePop,
+        IndieElectro,
+        LoFi,
+    }),
+    EDM(EDMSubtype {
+        ProgressiveHouse,
+        BigRoom,
+        Dubstep,
+        TropicalHouse,
+        FutureBass,
+    }),
+    Latin(LatinSubtype {
+        Salsa,
+        Bachata,
+        Reggaeton,
+        Mariachi,
+        Tango,
+    }),
+    World(WorldSubtype {
+        African,
+        Caribbean,
+        MiddleEastern,
+        Asian,
+        Celtic,
+    }),
+}
---
+++ src/macros.rs
@@ -0,0 +1,27 @@
+#[macro_export]
+macro_rules! declare_subtype {
+    ($enum_name:ident { $($variant:ident),* $(,)? }) => {
+        #[derive(
+            Clone, Copy, PartialEq, Eq, PartialOrd, Ord,
+            RuntimeDebug, Encode, Decode, TypeInfo, MaxEncodedLen
+        )]
+        pub enum $enum_name {
+            $($variant),*
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! declare_music_genre {
+    ($($genre:ident($subtype:ident { $($variant:ident),* $(,)? }),)*) => {
+        $(declare_subtype!($subtype { $($variant),* });)*
+
+        #[derive(
+            Clone, Copy, PartialEq, Eq, PartialOrd, Ord,
+            RuntimeDebug, Encode, Decode, TypeInfo, MaxEncodedLen
+        )]
+        pub enum MusicGenre {
+            $($genre($subtype)),*
+        }
+    }
+}
