dd1973faaa51afd04f73dfdeefb701f86275093b
SailorSnoW <sailorsnow@pm.me>
2023-11-13 16:43:51 +0100

    fix wasm compilation

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
 pub mod macros;
 
 use codec::{Decode, Encode, MaxEncodedLen};
